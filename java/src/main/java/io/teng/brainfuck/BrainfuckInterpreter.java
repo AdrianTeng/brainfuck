3d70a9b3407691af282763b9017657c5bca03bd7
Adrian Teng <adrian@teng.io>
2015-01-14 00:45:10 +0000

    Java solution: Implement everything except byte reading, with hello world test

--- java/src/main/java/io/teng/brainfuck/BrainfuckInterpreter.java
+++ java/src/main/java/io/teng/brainfuck/BrainfuckInterpreter.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
-import java.util.List;
+import java.util.*;
 
 import static java.nio.file.Files.readAllLines;
 
@@ -15,6 +15,7 @@ public class BrainfuckInterpreter {
 
     private final String fileName;
     private final String arg;
+    private Map<Integer, Integer> bracketMap;
 
     public BrainfuckInterpreter(String fileName, String arg) {
         this.fileName = fileName;
@@ -32,7 +33,61 @@ public class BrainfuckInterpreter {
     }
 
 
-    public void interpret(){
+    public void interpret() {
+        String source;
+        List<Integer> heap = new ArrayList<Integer>(Collections.nCopies(100, 0));
+        int dataPtr = 0;
+        try {
+            source = parseFile();
+        } catch (IOException e) {
+            throw new RuntimeException("Unable to read source file.", e);
+        }
+        Map<Integer, Integer> bracketMap = genBracketMap(source);
+        for (int i=0; i < source.length(); i++) {
+            char c = source.charAt(i);
+            switch (c){
+                case '>':
+                    dataPtr++; break;
+                case '<':
+                    dataPtr--; break;
+                case '+':
+                    heap.set(dataPtr, heap.get(dataPtr) + 1); break;
+                case '-':
+                    heap.set(dataPtr, heap.get(dataPtr) - 1); break;
+                case '[':
+                    if (heap.get(dataPtr) == 0)
+                        i = bracketMap.get(i);
+                    break;
+                case ']':
+                    if (heap.get(dataPtr) != 0) {
+                        for (Map.Entry<Integer, Integer> entry: bracketMap.entrySet()) {
+                            if (i == entry.getValue()) {
+                                i = entry.getKey();
+                                break;
+                            }
+                        }
+                    }
+                    break;
+                case '.':
+                    System.out.printf("%s", (char) heap.get(dataPtr).intValue()); break;
+            }
+        }
+
+    }
+
+    private Map<Integer, Integer> genBracketMap(String source){
+        Map<Integer, Integer> bracketMap = new HashMap<Integer, Integer>();
+        Stack<Integer> tempBracketStack = new Stack<Integer>();
+        for (Integer i=0; i < source.length(); i++) {
+            char c = source.charAt(i);
+            switch (c){
+                case '[':
+                    tempBracketStack.push(i); break;
+                case ']':
+                    bracketMap.put(tempBracketStack.pop(), i); break;
+            }
+        }
+        return bracketMap;
     }
 
 
@@ -44,7 +99,6 @@ public class BrainfuckInterpreter {
             arg = args[1];
         }
         String fileName = args[0];
-        new BrainfuckInterpreter(fileName, arg).parseFile();
-
+        new BrainfuckInterpreter(fileName, arg).interpret();
     }
 }
---
+++ java/src/test/java/io/teng/brainfuck/testBrainfuckInterpreter.java
@@ -0,0 +1,23 @@
+package io.teng.brainfuck;
+
+import junit.framework.Assert;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by ateng on 01/01/15.
+ */
+public class testBrainfuckInterpreter {
+
+    @Test
+    public void testHelloWorld(){
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(baos));
+        new BrainfuckInterpreter("../helloworld.bf", "").interpret();
+        assertEquals(baos.toString(), "Hello World!\n");
+    }
+}
