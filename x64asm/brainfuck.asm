f578f9b700bdfb9f3bb189562fef42b3b640f7ab
Adrian Teng <adrian@teng.io>
2015-02-04 01:01:53 +0000

    Add gen bracket map

--- x64asm/brainfuck.asm
+++ x64asm/brainfuck.asm
@@ -84,7 +84,7 @@ _find_file_size:
     syscall                         # Move the file cursor back to start
 
 _alloc_mem:
-    mov     %r14, %rax
+    mov     %r14, %rdi
     call    malloc
 
     push    %rax                    # Keeping allocated memory location in stack to return later
@@ -100,6 +100,7 @@ _read_file_content:
 
 
     pop     %rax                    # Return address of copied file content
+    ret
 
 
     #######################################################################################################
@@ -124,6 +125,85 @@ malloc:
                                     # address of the allocated memory
     ret
 
+
+    #######################################
+    # %rdi: address to the source in heap #
+    #######################################
+
+gen_bracket_map:
+
+    # count number of '[' in
+    mov     $0, %rax
+    mov     $0, %rcx                # %rcx to count number of '['
+
+_count_bracket_loop:
+
+    mov     %rdi, %rbx              # Use %rbx to address the i-th character (i is stored in %rax)
+    add     %rax, %rbx
+    movzbl  (%rbx), %ebx            # Copy the character we want to %bl and pad with zeros
+    cmp     $EOF, %rbx              # end loop if EOF found
+    je      _end_count_bracket_loop
+
+    inc     %rax
+    cmp     $91, %rbx               # increment %rcx if we have a '[' character (=91 in ascii)
+    je      _is_bracket
+    jmp     _count_bracket_loop
+
+
+_is_bracket:
+    inc     %rcx
+    jmp     _count_bracket_loop
+
+_end_count_bracket_loop:
+    push    %rdi                    # Keep the address to the source code in stack for now
+    mov     %rcx, %rdi              # We now have number of '[' in %rcx, lets malloc that amount of memory
+
+    call    malloc
+
+    pop     %rdi
+    mov     %rax, %r10              # allocaed memory address now in %r10
+    mov     %rax, %r11              # Store an extra copy (not modifying) in %r11
+    mov     $0, %rax
+
+_build_bracket_map:
+
+    mov     %rdi, %rbx              # Use %rbx to address the i-th character (i is stored in %rax)
+    add     %rax, %rbx
+    movzbl  (%rbx), %ebx            # Copy the character we want to %bl and pad with zeros
+    cmp     $EOF, %rbx              # end loop if EOF found
+    je      _end_build_bracket_map
+
+    inc     %rax
+    cmp     $91, %rbx               # if we have '[', push the location to stack
+    je      _push_bracket
+    cmp     $93, %rbx               # if we have ']', pop the matching '[' from stack and store both in heap
+    je      _pop_bracket
+    jmp     _build_bracket_map
+
+_push_bracket:
+
+    push    %rax
+    jmp     _build_bracket_map
+
+_pop_bracket:                       # Given a pair of matched bracket positions (each as 32 bit int), combine
+                                    # them as a 64 bits and store in heap (where %r10 is pointing)
+
+    mov     %eax, %edx              # move the position of ']' into lower 32 bits of %rdx
+    pop     %rcx
+    shl     $8, %rcx                # now shift the position of '[' to higher 32 bits and combine them into %rdx
+    or      %rcx, %rdx
+
+    mov     %rdx, (%r10)
+    add     $2, %r10
+
+    jmp     _build_bracket_map
+
+_end_build_bracket_map:
+
+    mov     %r11, %rax
+    ret
+
+
 _start:
 
     mov     16(%rsp), %rdi          # %rsp = argc, then argv for each word
@@ -132,8 +212,15 @@ _start:
     mov     16(%rsp), %rdi
     call    read_file
 
+    push    %rax
+
     mov     %rax, %rdi
     call    print
+
+    pop     %rdi
+    call    gen_bracket_map
+
+debug:
 
     mov     %rax, %rdi              # string length is pass as exit code
     mov     $60, %rax               # system call 60 is exit
